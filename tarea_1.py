8a27fba55b28c703bfb146bcffacc096760f2514
Desfl14 <danielescriba.f@outlook.com>
2022-11-07 00:16:27 -0500

    Impresion de opciones y mas funciones

--- tarea_1.py
+++ tarea_1.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 class Libro:
     def __init__(self,id,titulo,genero,isbn,editorial,autor_es=[]) -> None:
@@ -20,18 +21,18 @@ class Libro:
 def intro():
     print('\n\tBienvenido al Registro de Libros')
     print('\nElige una de estas opciones:\n')
-    print('1 -> Leer archivo \n2 -> Listar libros\n3 -> Agregar libro\n4 -> Eliminar libro\n5 -> Buscar libro por ISBN o por título\n6 -> Ordenar libros por título\n7 -> Buscar libros por autor, editorial o género\n8 -> Buscar libros por número de autores\n9 -> Editar o actualizar datos de un libro\n10 -> Guardar libros en archivo\n')
+    print('1 -> Leer archivo \n2 -> Listar libros\n3 -> Agregar libro\n4 -> Eliminar libro\n5 -> Buscar libro por ISBN o por título\n6 -> Ordenar libros por título\n7 -> Buscar libros por autor, editorial o género\n8 -> Buscar libros por número de autores\n9 -> Editar o actualizar datos de un libro\n10 -> Guardar libros en archivo\n11-> Salir')
 
     while True:
         try:
             eleccion=int(input('Eliga el [NUMERO] a Eleccion :  '))
-            while eleccion not in (1,2,3,4,5,6,7,8,9,10):
+            while eleccion not in (1,2,3,4,5,6,7,8,9,10,11):
                 eleccion=int(input('Eliga el [NUMERO] a Eleccion : '))
             break
         except ValueError:
              print('Solo numeros!')
 
-    listaintro=['leer','listar','add','remove','bisbn','otitulos','bautor','nautor','edit','save']
+    listaintro=['leer','listar','add','remove','bisbn','otitulos','bautor','nautor','edit','save','salir']
     return listaintro[eleccion-1]
 
 
@@ -50,7 +51,7 @@ def abrir_fichero():
 def listarlibros(argumeto):
     for i in argumeto:
         print(i)
-        
+
 def agregarlibro(id,List3):
 
     autores=[]   
@@ -92,3 +93,100 @@ def sacar_libro(lista2):
         a=a+1
 
     return lista2
+
+def Buscar_libro(listb):
+    print("Buscar em datos")
+
+    buscar=input('numero de id a retirar: ')
+
+    a=0
+    for item in listb:
+
+        if (listb[a].titulo or listb[a].ISBM)==buscar:
+          print(listb[a])  
+        a=a+1
+
+
+def ordenar_libros(listb):
+    orden=(sorted(listb,key=lambda x: x.titulo))
+    for i in orden:
+        print(i)
+   
+
+def guardar_archivo(listb):
+    nom_fichero=input("ingrese el nombre del archivo->  ")
+    f = open(nom_fichero,'w') 
+
+    a=0
+    for item in listb:
+        b=f"{listb[a].id},{listb[a].titulo},{listb[a].isbn},{listb[a].editorial},{','.join(listb[a].autor_es)}"
+        f.write(b)
+        f.write("\n")
+        a=a+1
+    f.close
+
+
+
+while True:
+
+    inicio=intro()
+
+    while inicio != 'salir':
+
+        if inicio == 'leer':
+            lib=abrir_fichero()
+            input("\nPresione [Enter] para continuar... ")
+            os.system('cls')
+            break
+                
+        elif inicio == 'listar':
+            listarlibros(lib)
+            input("\nPresione [Enter] para continuar... ")
+            os.system('cls')
+            break
+            
+        elif inicio == 'add':
+            id=input('Ingrese el ID :')
+            agregarlibro(id,lib)
+            input("\nPresione [Enter] para continuar... ")
+            os.system('cls')
+            break
+
+        elif inicio == 'remove':
+            sacar_libro(lib)
+            input("\nPresione [Enter] para continuar... ")
+            os.system('cls')
+            break
+
+        elif inicio == 'bisbn':
+            Buscar_libro(lib)
+            input("\nPresione [Enter] para continuar... ")
+            os.system('cls')
+            break
+
+        elif inicio == 'otitulos':
+            ordenar_libros(lib)
+            input("\nPresione [Enter] para continuar... ")
+            os.system('cls')
+            break
+
+        elif inicio == 'bautor':
+            pass
+
+        elif inicio == 'nautor':
+            pass
+
+        elif inicio == 'edit':
+            pass
+
+        elif inicio == 'save':
+            guardar_archivo(lib)
+            input("\nPresione [Enter] para continuar... ")
+            os.system('cls')
+            break    
+    
+    if inicio=='salir':
+        break
+
+
+
